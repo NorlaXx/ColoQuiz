64e74b4f149f722e2a1c679f01b71b30ebbd978e
NorlaXx <alexis.rochefort35@gmail.com>
2022-01-20 14:17:59 +0100

    Initial commit

---
+++ questions.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var questions_json_1 = require("./questions.json");
+console.log(questions_json_1["default"].quiz.Age.de_4_à_9_ans.Thème.Nature.Question1);
+console.log(questions_json_1["default"].Réponses.Question1[0]);
---
+++ questions.ts
@@ -0,0 +1,4 @@
+import questions from "./questions.json"
+
+console.log(questions.quiz.Age.de_4_à_9_ans.Thème.Nature.Question1)
+console.log(questions.Réponses.Question1[0])
