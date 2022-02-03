971f5bdf65bf2cf95255e958b6852ce01b05783c
NorlaXx <alexis.rochefort35@gmail.com>
2022-02-03 17:10:03 +0100

    change the ts file for display questions and answer on the html file and add form to the html file

--- questions.js
+++ questions.js
@@ -4,6 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const questions_json_1 = __importDefault(require("./questions.json"));
-console.log(questions_json_1.default.Thème.Nature[0].Question);
-console.log(questions_json_1.default.Thème.JeuxVidéos[1].GoodAwnser);
-console.log(questions_json_1.default.Thème.Films[1].Reponses[1]);
+// console.log(questions.Thème.Nature[0].Question)
+// console.log(questions.Thème.JeuxVidéos[1].GoodAwnser)
+// console.log(questions.Thème.Films[1].Reponses[1])
+// Affichage de la question
+let Question = document.getElementById("Question");
+Question.innerHTML = questions_json_1.default.Films[0].Question;
+let Réponse1 = document.getElementById("Réponse1");
+Réponse1.innerHTML = questions_json_1.default.Films[0].Reponses[0];
+let Réponse2 = document.getElementById("Réponse2");
+Réponse2.innerHTML = questions_json_1.default.Films[0].Reponses[1];
+let Réponse3 = document.getElementById("Réponse3");
+Réponse3.innerHTML = questions_json_1.default.Films[0].Reponses[2];
+let Réponse4 = document.getElementById("Réponse4");
+Réponse4.innerHTML = questions_json_1.default.Films[0].Reponses[3];
--- questions.ts
+++ questions.ts
@@ -1,5 +1,21 @@
 import questions from "./questions.json"
 
-console.log(questions.Thème.Nature[0].Question)
-console.log(questions.Thème.JeuxVidéos[1].GoodAwnser)
-console.log(questions.Thème.Films[1].Reponses[1])
+// console.log(questions.Thème.Nature[0].Question)
+// console.log(questions.Thème.JeuxVidéos[1].GoodAwnser)
+// console.log(questions.Thème.Films[1].Reponses[1])
+
+// Affichage de la question
+let Question:any = document.getElementById("Question") as HTMLElement
+Question.innerHTML = questions.Films[0].Question
+
+let Réponse1:any = document.getElementById("Réponse1") as HTMLElement
+Réponse1.innerHTML = questions.Films[0].Reponses[0]
+
+let Réponse2:any = document.getElementById("Réponse2") as HTMLElement
+Réponse2.innerHTML = questions.Films[0].Reponses[1]
+
+let Réponse3:any = document.getElementById("Réponse3") as HTMLElement
+Réponse3.innerHTML = questions.Films[0].Reponses[2]
+
+let Réponse4:any = document.getElementById("Réponse4") as HTMLElement
+Réponse4.innerHTML = questions.Films[0].Reponses[3]
