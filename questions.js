14ae7e689ccc3fe17d8455d127b455b0aded2604
NorlaXx <alexis.rochefort35@gmail.com>
2022-02-21 19:30:53 +0100

    Add array to put questions and answers

--- questions.js
+++ questions.js
@@ -7,17 +7,32 @@ const questions_json_1 = __importDefault(require("./questions.json"));
 // console.log(questions.Thème.Nature[0].Question)
 // console.log(questions.Thème.JeuxVidéos[1].GoodAwnser)
 // console.log(questions.Thème.Films[1].Reponses[1])
+// Variable du bouton submit
+const submit = document.getElementById("submit");
 // Nombre de questions de chaque thèmes
 var NbQuestionsFilms = questions_json_1.default.Films.length;
 console.log(NbQuestionsFilms);
 var NbQuestionsNature = questions_json_1.default.Nature.length;
 var NbQuestionsJeux = questions_json_1.default.JeuxVidéos.length;
-// Ajout des questions dans un tableau
+// Ajout des questions dans un tableau et des réponses dans un tableau
 var QuestionsFilms = [];
+var Reponses = [];
+// Ajout de tableaux dans le tableaux Reponses
+for (let i = 0; i < NbQuestionsFilms; i++) {
+    Reponses.push([]);
+}
+// Ajout des questions
 for (let i = 0; i < NbQuestionsFilms; i++) {
     QuestionsFilms.push(questions_json_1.default.Films[i].Question);
 }
+// Ajout des reponses
+for (let i = 0; i < NbQuestionsFilms; i++) {
+    for (let j = 0; j < 4; j++) {
+        Reponses[i].push(questions_json_1.default.Films[i].Reponses[j]);
+    }
+}
 console.log(QuestionsFilms);
+console.log(Reponses);
 // tirage aléatoire de n questions
 function AffichageAleatoire() {
     // Création  d'un tableau pour les questions déjà apparues
@@ -27,21 +42,39 @@ function AffichageAleatoire() {
     console.log(NbAleatoire);
     // Affichage de la question avec ses réponses
     let Question = document.getElementById("Question");
-    Question.innerHTML = questions_json_1.default.Films[NbAleatoire].Question;
+    Question.innerHTML = QuestionsFilms[NbAleatoire];
     let Réponse1 = document.getElementById("Réponse1");
-    Réponse1.innerHTML = questions_json_1.default.Films[NbAleatoire].Reponses[0];
+    Réponse1.innerHTML = Reponses[NbAleatoire][0];
     let Réponse2 = document.getElementById("Réponse2");
-    Réponse2.innerHTML = questions_json_1.default.Films[NbAleatoire].Reponses[1];
+    Réponse2.innerHTML = Reponses[NbAleatoire][1];
     let Réponse3 = document.getElementById("Réponse3");
-    Réponse3.innerHTML = questions_json_1.default.Films[NbAleatoire].Reponses[2];
+    Réponse3.innerHTML = Reponses[NbAleatoire][2];
     let Réponse4 = document.getElementById("Réponse4");
-    Réponse4.innerHTML = questions_json_1.default.Films[NbAleatoire].Reponses[3];
+    Réponse4.innerHTML = Reponses[NbAleatoire][3];
     // Ajout des questions déjà apparues dans le tableau prévu à cet effet
     QuestionsApparues.push(questions_json_1.default.Films[NbAleatoire].Question);
     console.log(QuestionsApparues);
     // Supprime les questions étant déjà apparues du tableau des questions
     QuestionsFilms.splice(NbAleatoire, 1);
     console.log(QuestionsFilms);
+    // Supprime les réponses déjà apparues du tableaux des reponses
+    Reponses.splice(NbAleatoire, 1);
 }
+// Fonction vérifiant si il y a envore des éléments dans le tableau des questions
+function ArrayEmpty() {
+    if (QuestionsFilms.length == 0) {
+        let quizEnd = document.getElementById("QuizEnd");
+        quizEnd.innerHTML = "C'est la fin du quiz, RETOUR";
+        quizEnd.href = "http://localhost:1234";
+    }
+    else {
+        return;
+    }
+}
+// Active la fonction AffichageAleatoire() et la fonction ArrayEmpty() lors du click sur le bouton Valider
+submit.onclick = function () {
+    AffichageAleatoire();
+    ArrayEmpty();
+};
 // Affiche aléatoirement la première question
 AffichageAleatoire();
--- questions.ts
+++ questions.ts
@@ -5,18 +5,34 @@ import questions from "./questions.json"
 // console.log(questions.Thème.JeuxVidéos[1].GoodAwnser)
 // console.log(questions.Thème.Films[1].Reponses[1])
 
+// Variable du bouton submit
+const submit:any = document.getElementById("submit") as HTMLElement
+
 // Nombre de questions de chaque thèmes
 var NbQuestionsFilms:number = questions.Films.length
 console.log(NbQuestionsFilms)
 var NbQuestionsNature:number = questions.Nature.length
 var NbQuestionsJeux:number = questions.JeuxVidéos.length
 
-// Ajout des questions dans un tableau
+// Ajout des questions dans un tableau et des réponses dans un tableau
 var QuestionsFilms:string[] = []
+var Reponses:any[] = []
+// Ajout de tableaux dans le tableaux Reponses
+for(let i = 0; i < NbQuestionsFilms; i++) {
+    Reponses.push([])
+}
+// Ajout des questions
 for(let i = 0; i < NbQuestionsFilms; i++) {
     QuestionsFilms.push(questions.Films[i].Question)
 }
+// Ajout des reponses
+for(let i = 0; i < NbQuestionsFilms; i++) {
+    for(let j = 0; j < 4; j++) {
+        Reponses[i].push(questions.Films[i].Reponses[j])
+    }
+}
 console.log(QuestionsFilms)
+console.log(Reponses)
 
 // tirage aléatoire de n questions
 
@@ -31,19 +47,19 @@ function AffichageAleatoire() {
 
     // Affichage de la question avec ses réponses
     let Question:any = document.getElementById("Question") as HTMLElement
-    Question.innerHTML = questions.Films[NbAleatoire].Question
+    Question.innerHTML = QuestionsFilms[NbAleatoire]
 
     let Réponse1:any = document.getElementById("Réponse1") as HTMLElement
-    Réponse1.innerHTML = questions.Films[NbAleatoire].Reponses[0]
+    Réponse1.innerHTML = Reponses[NbAleatoire][0]
 
     let Réponse2:any = document.getElementById("Réponse2") as HTMLElement
-    Réponse2.innerHTML = questions.Films[NbAleatoire].Reponses[1]
+    Réponse2.innerHTML = Reponses[NbAleatoire][1]
 
     let Réponse3:any = document.getElementById("Réponse3") as HTMLElement
-    Réponse3.innerHTML = questions.Films[NbAleatoire].Reponses[2]
+    Réponse3.innerHTML = Reponses[NbAleatoire][2]
 
     let Réponse4:any = document.getElementById("Réponse4") as HTMLElement
-    Réponse4.innerHTML = questions.Films[NbAleatoire].Reponses[3]
+    Réponse4.innerHTML = Reponses[NbAleatoire][3]
 
     // Ajout des questions déjà apparues dans le tableau prévu à cet effet
     QuestionsApparues.push(questions.Films[NbAleatoire].Question)
@@ -52,8 +68,28 @@ function AffichageAleatoire() {
     // Supprime les questions étant déjà apparues du tableau des questions
     QuestionsFilms.splice(NbAleatoire, 1)
     console.log(QuestionsFilms)
+
+    // Supprime les réponses déjà apparues du tableaux des reponses
+    Reponses.splice(NbAleatoire, 1)
 }
 
+// Fonction vérifiant si il y a envore des éléments dans le tableau des questions
+function ArrayEmpty() {
+    if(QuestionsFilms.length == 0) {
+        let quizEnd:any = document.getElementById("QuizEnd") as HTMLElement
+        quizEnd.innerHTML = "C'est la fin du quiz, RETOUR"
+        quizEnd.href = "http://localhost:1234"
+    }
+    else {
+        return
+    }
+}
+
+// Active la fonction AffichageAleatoire() et la fonction ArrayEmpty() lors du click sur le bouton Valider
+submit.onclick = function() {
+    AffichageAleatoire()
+    ArrayEmpty()
+}
 
 // Affiche aléatoirement la première question
 AffichageAleatoire()
